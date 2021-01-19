36d00f4844b8ea029cbfe5d2460ad095a4e0c706
Ronaldo <tarfao@hotmail.com>
2021-01-19 18:42:52 -0300

    backend em funcionamento com o graphql

---
+++ README.md
@@ -0,0 +1,14 @@
+# Colheita de maçãs
+
+## Desafio 
+
+* Duas pessoas, Marcelo e Carla, trabalham em uma fazenda de maçãs na qual existem N árvores. Asárvores estão alinhadas em uma fileira e são numeradas de 1 a N. Marcelo planeja coletar maçãs deK árvores consecutivas e Carla de L árvores consecutivas. Eles querem escolher segmentosdisjuntos, ou seja, que não se sobrepõem para não interferirem na coleta do outro. Qual o maiornúmero de maçãs que os dois juntos podem coletar?
+
+*  Dado um vetor A = [3, 4, 1, 7, 8, 5], K = 2, L = 3, sua função deve retornar27, já que Carla escolherá as árvores 4, 5 e 6 e coletar 7 + 8 + 5 = 20 maçãs. Marcelo, por sua vez,escolherá as árvores 1 e 2, coletando assim 3 + 4 = 7 maçãs.
+
+## Tecnologia
+
+* Python
+* Flask
+* Graphql
+* VueJS
---
+++ backend.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 18 17:31:20 2021
+
+@author: tarfa
+"""
+
+from flask import Flask, request
+import graphene
+import json
+
+app = Flask(__name__)
+
+'''
+OBJETIVO: ENCONTRAR A MELHOR COMBINAÇÃO SEQUENCIAL DE PÉS DE MAÇÃS QUE POSSUI A MAIOR SOMA
+PARÂMETROS: 
+    * A = UM ARRAY QUE INDICA O NUMERO DE MAÇÃS EM CADA ÁRVORE CONSECUTIVA
+    * N_TREE = O NUMERO DE ÁRVORES QUE SERÃO COLHIDAS POR CARLA OU MARCELO
+    * LAST_INIT_COUNT = REPRESENTA O INÍCIO DA COLETA DA ÚLTIMA PESSOA 
+                        AVALIADA (CARLA OU MARCELO), POIS NAO PODEM COLETAR MAÇÃS 
+                        DE UMA MESMA ÁRVORE. CASO NINGUEM FOI AVALIADO ANTERIORMENTE
+                        O VALOR INICIAL É 0
+    * LAST_N_TREE = REPRESENTA O NÚMERO DE ARVORES ANALISADAS ANTERIORMENTE, CASO
+                    NENHUMA FOI LIDA, O VALOR É 0
+RETORNO: UMA LISTA CONTENDO DOIS NÚMEROS: 
+        1 - O NÚMERO MÁXIMO DE MAÇÃS COLHIDAS PELA PESSOA ENVIDA,
+        2 - E O INÍCIO DA COLETA, SE COMEÇARÁ PELO PÉ 0,1,2,3...
+'''
+def max_apples_un(A, N_TREE, LAST_INIT_COUNT = 0, LAST_N_TREE = 0):
+    '''INICIALMENTE O NUMERO MAXIMO DE MAÇÃS A SEREM COLETADAS É -1, JÁ O 
+    INICIO DA CONTAGEM É A PARTIR DA POSIÇÃO INICIAL, 0'''
+    MAX_APPLES = -1
+    INIT_COUNT = 0
+    INIT_COUNT_PERSON = -1 #assumimos que o inicio da coleta da pessoa atual é inválido
+    if(LAST_INIT_COUNT == 0):#SIGNIFICA QUE TODAS AS ARVORES PODEM SER AVALIADAS
+        while(INIT_COUNT+N_TREE <= len(A)):
+            acumulador = 0
+            for i in range(INIT_COUNT, INIT_COUNT+N_TREE):
+                acumulador += A[i]
+            if(MAX_APPLES < acumulador):
+                MAX_APPLES = acumulador
+                INIT_COUNT_PERSON = INIT_COUNT
+            INIT_COUNT += 1
+    else: #SE NAO, SIGNIFICA QUE ESTAMO ANALISANDO A SEGUNDA COLHEITA
+        '''ENQUANTO HOUVER A POSSIBILIDADE DE ANALISAR O CONJUNTO DE ÁRVORES QUE SOBRARAM, 
+           ANTES DE ALCANÇAR O INÍCIO DAS ÁRVORES ESCOLHIDAS PARA A PESSOA ANTERIOR, VAMOS ANALISA-LAS'''
+        while(INIT_COUNT+N_TREE <= LAST_INIT_COUNT):
+            acumulador = 0
+            for i in range(INIT_COUNT, INIT_COUNT+N_TREE):
+                acumulador += A[i]
+            if(MAX_APPLES < acumulador):
+                MAX_APPLES = acumulador
+                INIT_COUNT_PERSON = INIT_COUNT
+            INIT_COUNT += 1
+        #INICIAMOS AGORA O SUBCONJUNTO APÓS O SUBCONJUNTO DA PESSOA ANTERIOR
+        INIT_COUNT = LAST_INIT_COUNT + LAST_N_TREE
+        #caso haja possibilidade de analisar árvores após as árvores escolhidas para a pessoa anterior
+        while(INIT_COUNT+N_TREE <= len(A)):
+            acumulador = 0
+            for i in range(INIT_COUNT, INIT_COUNT+N_TREE):
+                acumulador += A[i]
+            if(MAX_APPLES < acumulador):
+                MAX_APPLES = acumulador
+                INIT_COUNT_PERSON = INIT_COUNT
+            INIT_COUNT += 1
+    return [MAX_APPLES, INIT_COUNT_PERSON]
+
+'''
+OBJETIVO: ENCONTRAR O NÚMERO MÁXIMO DE MAÇÃS COLHIDAS POR MARCELO E CARLA
+PARÂMETROS: 
+    * A -> UMA LISTA CONTENDO A QUANTIDADE DE MAÇÃS DE CADA PÉ, RESPECTIVAMENTE.
+    * K -> NÚMERO DE PÉS QUE SERÃO ESCOLHIDOS PARA O MARCELO
+    * L -> NÚMERO DE PÉS QUE SERÃO COLHIDOS POR CARLA
+RETORNO: UMA LISTA COM 3 VALORES QUE REPRESENTAM
+        1 - A MAIOR QUANTIDADE DE MAÇÃS QUE AMBOS PODERÃO COLHER
+        2 - O PRIMEIRO PÉ DE MAÇÃ QUE O MARCELO IRÁ COLHER
+        3 - O PRIMEIRO PÉ DE MAÇÃ QUE CARLA IRÁ COLHER
+'''
+def get_max_apples(A,K,L):
+    if(len(A) < (K+L)): #COLHEITA INVÁLIDA!
+        return [-1, -1, -1]
+    if(L >= K):
+            [N_APPLES_C,INIT_TREE_C]=max_apples_un(A, L)
+            [N_APPLES_M,INIT_TREE_M]=max_apples_un(A, K, INIT_TREE_C, L)
+    else:
+        [N_APPLES_M,INIT_TREE_M]=max_apples_un(A, K)
+        [N_APPLES_C,INIT_TREE_C]=max_apples_un(A, L, INIT_TREE_M, K)
+    return [N_APPLES_M + N_APPLES_C, INIT_TREE_M, INIT_TREE_C]
+
+class Query(graphene.ObjectType):
+    max_apples = graphene.String(
+            trees=graphene.String(required=True),
+            n_marcelo=graphene.Int(required=True),
+            n_carla=graphene.Int(required=True)
+    )
+    
+    def resolve_max_apples(self, info, trees, n_marcelo, n_carla):
+        list_str_tree = trees.split(",") #separo os números enviados por vigula, pois estará no formato "1,2,3,4"
+        list_int_tree = list(map(int, list_str_tree)) #transformo a lista de string, em uma lista de int
+        data_apples = get_max_apples(list_int_tree, n_marcelo, n_carla)
+        return (data_apples)
+
+'''def main():
+    result = schema.execute('{ maxApples(trees: "3,4,1,7,8,5", nMarcelo: 3, nCarla: 2) }' )
+    print(result)'''
+
+schema = graphene.Schema(query=Query)
+
+@app.route("/", methods=['POST'])
+def apple_harvest():
+    data = json.loads(request.data)
+    print(data['query']);
+    return json.dumps(schema.execute(data['query']).data)
+                
