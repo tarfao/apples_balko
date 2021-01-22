1c3eb96111781fbf2b1befead396c67ec0641f6d
Ronaldo <tarfao@hotmail.com>
2021-01-22 13:16:21 -0300

    atualizando os dados no store, e exibindo-os na segunda página

--- frontend/src/views/Resultado/index.vue
+++ frontend/src/views/Resultado/index.vue
@@ -3,6 +3,9 @@
     <h1 v-on:click="cons">Segunda página</h1>
     <h2>{{this.$store.state.nMarcelo}}</h2>
     <h2>{{this.$store.state.nCarla}}</h2>
+    <h3 v-for="apples in this.$store.state.trees" :key="apples">
+        {{apples}}
+    </h3>
   </div>
 </template>
 
