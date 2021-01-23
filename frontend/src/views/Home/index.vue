e4b5d2cf5cbfb7407546604588211d1e420465ef
Ronaldo <tarfao@hotmail.com>
2021-01-23 11:52:42 -0300

    adicionando validação de entrada de dados!

--- frontend/src/views/Home/index.vue
+++ frontend/src/views/Home/index.vue
@@ -5,21 +5,19 @@
       <div class="container-input">
         <label>Vetor de árvores:</label>
         <input class="input-field" v-model="trees" type="text" />
+        <p v-if="treesError" class="error-p">Exemplo de entrada: 3,2,5,4,3</p>
       </div>
       <div class="container-input">
         <label>Árvores de Marcelo:</label>
         <input class="input-field" v-model="nMarcelo" type="number" />
+        <p v-if="nMarceloError" class="error-p">O número deve ser maior que 0.</p>
       </div>
       <div class="container-input">
         <label>Árvores de Carla:</label>
         <input class="input-field" v-model="nCarla" type="number" />
+        <p v-if="nCarlaError" class="error-p">O número deve ser maior que 0.</p>
       </div>
-      <div 
-      v-on:click="calcula" 
-      class="button-default"
-      >
-      Calcular
-      </div>
+      <div v-on:click="calcula" class="button-default">Calcular</div>
     </div>
   </div>
 </template>
@@ -33,33 +31,49 @@ export default {
   data() {
     return {
       trees: "",
-      nMarcelo: 0,
-      nCarla: 0,
+      nMarcelo: 1,
+      nCarla: 1,
     };
+  },
+  computed:{
+    treesError() {
+      return !/(\d(,\d)*)$/.test(this.trees) && this.trees !== ""
+    },
+    nMarceloError(){
+      return !(this.nMarcelo > 0)
+    },
+    nCarlaError(){
+      return !(this.nCarla > 0)
+    },
   },
   methods: {
     async calcula() {
       //console.log(this.$apollo.queries.maxApples.start());
       try {
-        const { data } = await axios.post("http://localhost:5000/graphql", {
-          query: `
+        if (
+          /(\d(,\d)*)$/.test(this.trees) &&
+          this.nMarcelo > 0 &&
+          this.nCarla > 0
+        ) {
+          const { data } = await axios.post("http://localhost:5000/graphql", {
+            query: `
             {
               maxApples(trees: "${this.trees}", nMarcelo: ${this.nMarcelo}, nCarla: ${this.nCarla})
             }
           `,
-        });
-        const array_trees = this.trees.split(",").map((n) => parseInt(n));
-        const res = data.maxApples.split(",").map(n => parseInt(n))
-        this.$router.push("resultado");
-        this.$store.commit(
-          "changeValues", 
-          { 
-            trees: array_trees, 
-            nMarcelo: this.nMarcelo, 
+          });
+          const array_trees = this.trees.split(",").map((n) => parseInt(n));
+          const res = data.maxApples.split(",").map((n) => parseInt(n));
+          this.$router.push("resultado");
+          this.$store.commit("changeValues", {
+            trees: array_trees,
+            nMarcelo: this.nMarcelo,
             nCarla: this.nCarla,
-            res
-          }
-        )
+            res,
+          });
+        } else {
+          alert("Erro nos dados de entrada");
+        }
       } catch (error) {
         throw new Error(error);
       }
